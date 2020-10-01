b75f4847128af6b0838feb89a7fd7af8995985a1
yaypixxo <viggo@lekdorf.com>
2020-10-01 22:39:40 +0200

    Initial Commit

---
+++ Guess The Number/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Guess The Number
+//
+//  Created by Viggo Lekdorf on 01/10/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Guess The Number/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Guess The Number
+//
+//  Created by Viggo Lekdorf on 01/10/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
