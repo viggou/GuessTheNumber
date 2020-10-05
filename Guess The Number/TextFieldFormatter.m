4479a581d308b162ee3fa740f24954ceb8331c2d
yaypixxo <viggo@lekdorf.com>
2020-10-05 13:01:16 +0200

    limit text input to both negative and positive ints

--- Guess The Number/TextFieldFormatter.m
+++ Guess The Number/TextFieldFormatter.m
@@ -14,7 +14,33 @@
         return YES;
     }
     
-    NSScanner *scanner = [NSScanner scannerWithString:partialString];
+    NSString *stringFromSecondChar;
+    
+    NSString *firstChar;
+    
+    if ([partialString length] > 1) {
+        stringFromSecondChar = [partialString substringFromIndex:1];
+    }
+    else {
+        stringFromSecondChar = partialString;
+    }
+    
+    NSScanner *scanner = [NSScanner scannerWithString:stringFromSecondChar];
+    
+    firstChar = [partialString substringToIndex:1];
+    
+    if ([partialString length] == 1 && [firstChar containsString:@"-"]) {
+        return YES;
+    }
+    
+    if ([partialString containsString:@"-"]) {
+        if (!([scanner scanInt:0] && [scanner isAtEnd])) {
+            return NO;
+        }
+        else {
+            return YES;
+        }
+    }
     
     if (!([scanner scanInt:0] && [scanner isAtEnd])) {
         return NO;
